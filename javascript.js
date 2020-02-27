7ce5a2308756166107f33c0f421ddc7b48cd85b5
Maryam-Azizi <azizi.m1390@gmail.com>
2020-02-27 20:55:05 +0330

    js code passages added

---
+++ javascript.js
@@ -0,0 +1,10 @@
+	function like(){
+	
+	var element=document.getElementById("sticker1");
+		element.classList.remove("display-none");
+	} 
+	function dislike(){ 
+		alert("You dislike this post"); 
+		var element=document.getElementById("sticker2"); 
+		element.classList.remove("display-none");
+	} 
